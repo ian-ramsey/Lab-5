8bbd4530e120185d4057a84731a790909d37f223
unknown <iramsey@WS-I3-30.WS-ACADBUS.EDU>
2016-09-12 07:42:35 -0700

    First Commit

---
+++ Lab-5/Loop2.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Loop2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class Loop2
+{
+    int[] nums = new int [100];
+    public Loop2(){
+    
+    }
+    public void loadArray(){
+        for (int i=0;i<100;i++)
+            nums[i] = (int)(Math.random()*100+1);
+    }
+    public void sortArray(){
+        Arrays.sort(nums);
+    }
+    public void printArray(){
+        for (int i=0;i<100;i++){
+            if (i%10==0)
+            System.out.println();
+            System.out.print("\t" + nums[i]);
+        }
+    }
+    public void avg(){
+        int sum = 0;
+        for (int i=0;i<100;i++){
+            sum += nums[i];
+        }
+        System.out.println("\n The average is " + sum/100.0);
+    }
+}
